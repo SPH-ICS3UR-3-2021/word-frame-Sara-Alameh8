bca6af6d1a7c2b81c57840814d546714e9bc4399
Sara-Alameh8 <82894102+Sara-Alameh8@users.noreply.github.com>
2021-05-09 15:54:34 -0400

    Update!!!

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -5,13 +5,37 @@
  */
 package Main;
 
+import java.util.Scanner; 
 /**
  *
  * @author k_zeb
  */
 public class Main {
     public static void main(String[] args){
-        
+    Scanner in = new Scanner(System.in);
+    String word = in.nextLine().toUpperCase();
+    System.out.print("* ");
+    for (int i=0;i<word.length();i++) {
+        System.out.print(word.charAt(i)+" ");
     }
-    
+    System.out.println("*");
+    //divider (top to middle)
+    for (int i=0;i<word.length();i++) {
+        System.out.print(word.charAt(word.length()-(i+1))+" ");
+    for (int x=0;x<word.length();x++) {
+        System.out.print("* "); 
+    }
+    System.out.println(word.charAt(i));
+    }
+    //divider (middle to bottom)
+    System.out.print("* ");
+    for (int i = word.length()-1;i>=0;i--) {
+        System.out.print(word.charAt(i)+" ");
+    }
+    System.out.println("*");
+    //divider (end)
+    in.close(); 
+
+    } 
+
 }
